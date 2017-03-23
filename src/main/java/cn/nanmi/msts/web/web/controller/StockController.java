34753fa063db37c87dc1dfb533b475312c0c28ff
bin.jiang <bin.jiang@nan-mi.cn>
2017-03-23 09:47:25 +0800

    增加用户登录校验

--- src/main/java/cn/nanmi/msts/web/web/controller/StockController.java
+++ src/main/java/cn/nanmi/msts/web/web/controller/StockController.java
@@ -79,10 +79,10 @@ public class StockController {
         if (queryVO.getPageNo() < 0 || queryVO.getPageSize() < 0) {
             return new CSPageResponse(ErrorCode.FAIL_INVALID_PARAMS);
         }
-//        HttpSession session = request.getSession();
-//        UserDTO user = (UserDTO) session.getAttribute(ConstantHelper.USER_SESSION);
-        UserDTO user = new UserDTO();
-        user.setUserId(47L);
+        HttpSession session = request.getSession();
+        UserDTO user = (UserDTO) session.getAttribute(ConstantHelper.USER_SESSION);
+//        UserDTO user = new UserDTO();
+//        user.setUserId(47L);
         if (user == null) {
             return new CSResponse(ErrorCode.SESSION_ERROR);
         }
